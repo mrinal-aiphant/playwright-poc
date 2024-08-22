a085f794be86c2144217f7d13910ba9669117577
Mrinal Singh <mrinal@Shyams-MacBook-Pro-2.local>
2024-08-22 12:29:45 +0530

    initial

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -33,7 +33,7 @@ test('should have a title', async ({ page, context }) => {
     
     await page.waitForLoadState('domcontentloaded');
     console.log(await page.title());
-    await expect(page).toHaveTitle("Create Next sApp");
+    await expect(page).toHaveTitle("Create Next App");
   } catch (error) {
     // Wait for the video file to be written
     await new Promise(resolve => setTimeout(resolve, 2000));
@@ -45,5 +45,34 @@ test('should have a title', async ({ page, context }) => {
       console.log('No video found for this test.');
     }
     throw error; // Re-throw the error after attempting to retrieve the video
+  }
+});
+
+test('should redirect to Vercel when clicking the "By Vercel" button', async ({ page, context }) => {
+  try {
+    await page.goto('http://localhost:3000');
+    
+    // Refine the locator to be more specific
+    const vercelButton = page.locator('a[href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"]');
+    
+    // Click the button and wait for the new page to open
+    const [newPage] = await Promise.all([
+      context.waitForEvent('page'), // Wait for the new page to open
+      vercelButton.click(), // Trigger the action
+    ]);
+
+    // Wait for the new page to load and check the URL
+    await newPage.waitForLoadState('load');
+    await expect(newPage).toHaveURL('https://vercel.com/?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-ap');
+  } catch (error) {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    
+    const videoPath = path.join(videoDir, 'Vercel-redirect.mp4');
+    if (fs.existsSync(videoPath)) {
+      console.log(`Video saved at: ${videoPath}`);
+    } else {
+      console.log('No video found for this test.');
+    }
+    throw error;
   }
 });
